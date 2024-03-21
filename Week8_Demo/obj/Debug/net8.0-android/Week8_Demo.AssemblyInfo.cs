3c6c30c636502a554aab0da91d5a6c9dae65c68a
Teodor-Andrei Ramba <rambat@coventry.ac.uk>
2024-03-21 10:11:08 +0000

    test2

--- Week8_Demo/obj/Debug/net8.0-android/Week8_Demo.AssemblyInfo.cs
+++ Week8_Demo/obj/Debug/net8.0-android/Week8_Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Week8_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b215e266a649db7f3ef0127d20209d0e9d320c4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Week8_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Week8_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
