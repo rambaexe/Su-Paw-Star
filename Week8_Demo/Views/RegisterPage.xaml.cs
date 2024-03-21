4d142d16eb43da80e96b14f8e89b9101871dbc3b
Teodor-Andrei Ramba <rambat@coventry.ac.uk>
2024-03-21 18:02:41 +0000

    Authentication done (? hopefully). Working on Registration now.

---
+++ Week8_Demo/Views/RegisterPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Mobile_Application.Views;
+
+public partial class RegisterPage : ContentPage
+{
+	public RegisterPage()
+	{
+		InitializeComponent();
+	}
+}
--- Week8_Demo/obj/Debug/net8.0-android/Week8_Demo.AssemblyInfo.cs
+++ Week8_Demo/obj/Debug/net8.0-android/Week8_Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Week8_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1a14b490e070f7f9ed479b9076d34ee6f6643485")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+035f68e72fc16ed6c0a613edc0b7cac64cca25ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Week8_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Week8_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
