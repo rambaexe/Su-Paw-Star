bc78e132a35faca3c1638488449bdec004846f4a
Teodor-Andrei Ramba <rambat@coventry.ac.uk>
2024-03-21 10:38:18 +0000

    test

--- Week8_Demo/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Week8_Demo.AssemblyInfo.cs
+++ Week8_Demo/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Week8_Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Week8_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+034e3dd2a691541b04900fe246116cd44bda9c41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+299ab36421c199d9814fe9418b02160acc467c99")]
 [assembly: System.Reflection.AssemblyProductAttribute("Week8_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Week8_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
