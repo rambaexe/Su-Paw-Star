e60911ecfff5eb05d8ce83951e7567578b1bb7f4
Teodor-Andrei Ramba <rambat@coventry.ac.uk>
2024-03-25 16:23:32 +0000

    UI almost done for the feature

--- Week8_Demo/obj/Debug/net8.0-ios/iossimulator-x64/Week8_Demo.AssemblyInfo.cs
+++ Week8_Demo/obj/Debug/net8.0-ios/iossimulator-x64/Week8_Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Week8_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+74ab41e3836bc7f92371dc36efa0bb361d2cd0b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6c80db6c5169c2d125189e92e8fe12d8bc3d694d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Week8_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Week8_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
